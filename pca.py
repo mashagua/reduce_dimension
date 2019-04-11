af1b00a66d7326ce69794e534ba7c36671f1329e
mashagua <993498039@qq.com>
2019-04-11 20:54:02 +0800

    Initial commit

---
+++ pca.py
@@ -0,0 +1,12 @@
+# * coding:utf-8 *
+#@author    :mashagua
+#@time      :2019/4/11 20:52
+#@File      :pca.py
+#@Software  :PyCharm
+from keras.datasets import mnist
+def pca():
+    pass
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+if __name__=='__main__':
+    pass
